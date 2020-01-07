0402d0fd3ae646335587a9139c0d40afe4d2c195
vipufo123 <279424390@qq.com>
2020-01-07 14:36:21 +0800

    update

---
+++ start.sh
@@ -0,0 +1,30 @@
+#coding:utf-8
+###################################################
+# File Name: start.sh
+# Author: Meng Zhao
+# mail: @
+# Created Time: 2020年01月02日 星期四 13时35分54秒
+#=============================================================
+export MODEL_DIR=/root/zhaomeng/google-BERT/uncased_L-12_H-768_A-12
+#export MODEL_DIR=/root/zhaomeng/google-BERT/chinese_L-12_H-768_A-12
+#export MODEL_DIR=/root/zhaomeng/baidu_ERNIE/pad_to_tf/checkpoints
+#export MODEL_DIR=/home/zhaomeng/roBerta_model/HIT
+
+export DATA_DIR=/root/zhaomeng/dst_test/dstc8-schema-guided-dialogue
+
+export OUTPUT_DIR=output
+
+export OUTPUT_EMBEDDING_DIR=embeddings
+
+export OUTPUT_CKPT_DIR=output/checkpoints
+
+python -m baseline.train_and_predict \
+        --bert_ckpt_dir $MODEL_DIR \
+        --dstc8_data_dir $DATA_DIR \
+        --dialogues_example_dir $OUTPUT_DIR \
+        --schema_embedding_dir $OUTPUT_EMBEDDING_DIR \
+        --output_dir $OUTPUT_CKPT_DIR \
+        --dataset_split train \
+        --run_mode train \
+        --task_name dstc8_single_domain \
+        --do_lower_case True
---
+++ start_eval.sh
@@ -0,0 +1,26 @@
+#coding:utf-8
+###################################################
+# File Name: start.sh
+# Author: Meng Zhao
+# mail: @
+# Created Time: 2020年01月02日 星期四 13时35分54秒
+#=============================================================
+export MODEL_DIR=/root/zhaomeng/google-BERT/uncased_L-12_H-768_A-12
+#export MODEL_DIR=/root/zhaomeng/google-BERT/chinese_L-12_H-768_A-12
+#export MODEL_DIR=/root/zhaomeng/baidu_ERNIE/pad_to_tf/checkpoints
+#export MODEL_DIR=/home/zhaomeng/roBerta_model/HIT
+
+export DATA_DIR=/root/zhaomeng/dst_test/dstc8-schema-guided-dialogue
+
+export OUTPUT_DIR=output
+
+#10000,20000,103235
+export CKPT_NUM=103235
+
+export PREDICTION_DIR=$OUTPUT_DIR/checkpoints/pred_res_"$CKPT_NUM"_dev_dstc8_single_domain_dstc8-schema-guided-dialogue
+
+python -m evaluate \
+        --dstc8_data_dir $DATA_DIR \
+        --prediction_dir $PREDICTION_DIR \
+        --eval_set dev \
+        --output_metric_file $OUTPUT_DIR/output_metric_file
